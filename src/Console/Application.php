9528f9b12b8d2abc1c175d7bde95c6121d1a357d
smoench <simon.moench@sensiolabs.de>
2018-12-04 17:26:09 +0100

    use application version for caching

---
+++ src/Console/Application.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SensioLabs\Deptrac\Console;
+
+use SensioLabs\Deptrac\Console\Command\AnalyzeCommand;
+use SensioLabs\Deptrac\Console\Command\InitCommand;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Console\Application as BaseApplication;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+final class Application extends BaseApplication
+{
+    public const VERSION = '@git-version@';
+
+    public function __construct()
+    {
+        parent::__construct('deptrac', self::VERSION);
+
+        $container = new ContainerBuilder();
+        (new XmlFileLoader($container, new FileLocator(__DIR__)))->load(__DIR__.'/../../services.xml');
+        $container->compile();
+
+        $this->addCommands([
+            $container->get(InitCommand::class),
+            $container->get(AnalyzeCommand::class),
+        ]);
+
+        $this->setDefaultCommand('analyze');
+    }
+}
